07a68f65608a553cfb8e6bf119519f82f823fd51
Francisco <franciscogatica180@gmail.com>
2026-03-27 20:25:46 -0300

    Project files

---
+++ src/main/java/com/mycompany/tema38/Tema38.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tema38;
+
+/**
+ *
+ * @author franc
+ */
+public class Tema38 {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al sistema de Arrendamiento de Equipos y Herramientas para Red de Sucursales");
+    }
+}
